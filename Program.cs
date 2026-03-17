20be684829247f2c55d7455fdbcb21b22871df54
Odododueshuk <xuph9ma@gmail.com>
2026-03-17 15:34:09 +0800

    11

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+
+namespace _20260317
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe,tom,marry;
+
+            joe=new Test();
+            marry =new Test();
+
+            joe.Mid = 80;
+            joe.Final = 90;
+
+            marry.Mid = 60;
+            marry.Final = 66;
+            
+            Console.WriteLine("joe"+joe.GetAvg());
+
+            tom = joe;
+
+            Console.WriteLine("tom"+tom.GetAvg());
+
+            joe.SetGrade(99, 100);
+
+            Console.WriteLine("tom"+tom.GetAvg());
+        }
+    }
+}
